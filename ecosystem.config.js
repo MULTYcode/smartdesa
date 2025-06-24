4e997e10a5316fdeb303bff78070fae682688c56
MULTYcode <mhdwasiman@gmail.com>
2025-06-24 09:35:37 +0700

    smartdesa : update pm2 config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,14 @@
 module.exports = {
-  apps : [{
-    name: "smartdesa-template-2",
-    script: "npm run start",
-    args: "start",
-    cwd: "/smartdesa",
-    autorestart: true,
-    watch: false,
-    max_memory_restart: '1G',
-    env: {
-      NODE_ENV: "production",
-      PORT: 3000
-    }
-  }]
+  apps: [
+    {
+      name: 'smartdesa-template-2',
+      script: 'node_modules/next/dist/bin/next',
+      args: 'start -p 3000',
+      instances: 'max',
+      exec_mode: 'cluster',
+      env: {
+        NODE_ENV: 'production',
+      },
+    },
+  ],
 };
